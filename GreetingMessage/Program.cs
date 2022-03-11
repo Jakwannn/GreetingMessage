16277095aabdc8804176a5c9e689a2f7a91ce6ad
Naufal Dzakwan <n.dzakwan9@gmail.com>
2022-03-12 03:30:09 +0700

    Add project files.

---
+++ GreetingMessage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreetingMessage
+{
+    /// <summary>
+    /// main class
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = Greet("Naufal Dzakwan");
+            Console.WriteLine(msg);
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Menampilkan pesan yang berupa nama
+        /// </summary>
+        /// <param name="name">Target nama</param>
+        /// <returns>Pesan nama</returns>
+        static string Greet(string name)
+        {
+            string msg = $"Hello {name}!";
+            return msg;
+        }
+    }
+}
